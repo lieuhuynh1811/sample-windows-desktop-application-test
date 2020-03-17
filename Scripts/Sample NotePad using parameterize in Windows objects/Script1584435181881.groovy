947228a44aef6716f9c3f7762f173af24533c2ac
lieuhuynh1811 <lieuhuynh@kms-technology.com>
2020-03-17 16:33:58 +0700

    internal commit

---
+++ Scripts/Sample NotePad using parameterize in Windows objects/Script1584435181881.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.testobject.WindowsTestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+'Start app'
+Windows.startApplication('C:\\Windows\\System32\\notepad.exe')
+
+'Edit file'
+Windows.setText(findWindowsObject('Object Repository/Edit',['Name' : Name]), 'Here is text create by Lieu Huynh')
+
+'Click X button'
+Windows.click(findWindowsObject('Object Repository/CloseButton',['nameofCloseButton' : nameofCloseButton]))
+
+'Close app'
+Windows.closeApplication()
